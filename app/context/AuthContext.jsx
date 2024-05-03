969df9a7e43d21deee72019001873522e52cd874
anujglassfrog <anujrawat@glassfrog.design>
2024-05-03 13:08:10 +0530

    contact us page udpated

---
+++ app/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { createContext, useContext, useState } from "react"
+
+const AuthContext = createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+let initialValues = {
+    isAuthenticated: false,
+    user: null
+};
+
+
+
+export function AuthProvider({ children }) {
+    const [userState, setUserState] = useState(initialValues)
+
+    const userlogin = () => {
+        const authuserState = { isAuthenticated: true, user: null }
+        setUserState(authuserState)
+    }
+
+    const userlogout = () => {
+        setUserState({
+            ...userState,
+            isAuthenticated: false,
+            user: null
+        })
+    }
+
+    const value = {
+        userState,
+        userlogin,
+        userlogout
+    }
+
+    return (
+        <>
+            <AuthContext.Provider value={value}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    )
+}
