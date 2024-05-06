0f1821aa79d9879338c99dcd746e78259a0fc6d1
amanglassfrog <aman@glassfrog.design>
2024-05-06 15:56:18 +0530

    fix

--- app/genie-resume/page.jsx
+++ app/genie-resume/page.jsx
@@ -1,31 +1,26 @@
-'use client'
-import React, { useEffect } from 'react'
-import { Resume } from '../components/Resume'
-import { Provider } from 'react-redux'
-import { store } from '../lib/redux/store'
+'use client';
+import React, { useEffect } from 'react';
+import { Resume } from '../components/Resume';
+import { Provider } from 'react-redux';
+import { store } from '../lib/redux/store';
 
-const page = () => {
-
-  
+const Page = () => {
   useEffect(() => {
     const resume = localStorage.getItem('newResumeContent');
 
-    if (resume ) {
-      //call api to get the json
-      //set that to resume-builder-parser-state inside its resume object
-     
+    if (resume) {
+      // Call API to get the JSON
+      // Set that to resume-builder-parser-state inside its resume object
     }
-
-  }, [])
-
+  }, []);
 
   return (
     <>
-    <Provider store={store}>
-    <Resume/>
-     </Provider>
+      <Provider store={store}>
+        <Resume />
+      </Provider>
     </>
-  )
+  );
 }
 
-export default page
+export default Page;
