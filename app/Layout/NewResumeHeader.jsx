e803b2ae8e47d6fa9cf20fcc9197ede5a531b52b
manoj7417 <manoj@glassfrog.design>
2024-05-07 13:10:53 +0530

    Changes in resume header

--- app/Layout/NewResumeHeader.jsx
+++ app/Layout/NewResumeHeader.jsx
@@ -147,7 +147,6 @@ export default function NewResumeHeader() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
