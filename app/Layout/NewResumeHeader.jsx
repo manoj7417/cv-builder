107236fee90dfe0499d914c3f385411202a1207b
manoj7417 <manoj@glassfrog.design>
2024-05-07 12:43:49 +0530

    Changes in New Header

--- app/Layout/NewResumeHeader.jsx
+++ app/Layout/NewResumeHeader.jsx
@@ -11,7 +11,7 @@ import { toast } from "react-toastify";
 const navigation = [
   { name: "Resume Builder", href: "/resume-builder", current: true },
   { name: "Resume Analyser", href: "/resumeAnalyzer-dashboard", current: false },
-  { name: "Genie Talk", href: "/genie-dashboard", current: false },
+  { name: "Genie Connect", href: "/genie-dashboard", current: false },
   { name: "Psychometric Test", href: "#", current: false },
 ];
 
