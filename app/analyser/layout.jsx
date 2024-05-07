282bce93843a8bcae87216ef5b444d4db50237e6
anujglassfrog <anujrawat@glassfrog.design>
2024-05-07 10:07:13 +0530

    analyser page issues fixed

---
+++ app/analyser/layout.jsx
@@ -0,0 +1,18 @@
+import Footer from "../Layout/Footer"
+import ResumeHeader from "../Layout/ResumeHeader"
+
+const Layout = ({ children }) => {
+    return (
+      <main className="resume_dashboard" >
+        <div className="resume_dashboard_container">
+          <div className="wrapper">
+            <ResumeHeader/>
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </main>
+    )
+  }
+  
+  export default Layout
