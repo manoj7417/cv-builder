130805836e3413a6621c999a0329fe374feac6c2
manoj7417 <manoj@glassfrog.design>
2024-05-20 13:04:42 +0530

    Changes in resume viewer page

--- app/resume-viewer/ResumeViewer.jsx
+++ app/resume-viewer/ResumeViewer.jsx
@@ -3,7 +3,7 @@ import Template3 from "@/components/resume-templates/Template3";
 import { cn } from "@/lib/utils";
 import { MagnifyingGlassIcon, ReloadIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CiUndo } from "react-icons/ci";
 import { FiPlus } from "react-icons/fi";
 import { FiMinus } from "react-icons/fi";
@@ -28,8 +28,6 @@ import { Button } from "../components/Button";
 import { printResume } from "../pages/api/api";
 import Link from "next/link";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
-import { GetTemplate } from "@/components/resume-templates/GetTemplate";
-import { metadata } from "../layout";
 
 const Controls = () => {
   const { zoomIn, zoomOut, resetTransform } = useControls();
@@ -58,12 +56,12 @@ const Controls = () => {
   );
 };
 
-const ResumeViewPage = ({ resumeData, setResumeData }) => {
+const ResumeViewPage = ({ resumeData }) => {
+  const [scale, setScale] = useState(0.8);
   const transformRef = useRef(null);
   const dropdownRef = useRef(null);
   const [isToggleOpen, setIsToggleOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
 
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -99,18 +97,6 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
     }
   };
 
-  const handleChangeTemplate = (name) => {
-    const updatedResumeData = {
-      ...resumeData, metadata: {
-        ...resumeData.metadata,
-        template: name
-      }
-    }
-    setResumeData(updatedResumeData)
-  }
-
-
-
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
@@ -137,7 +123,6 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
       window.removeEventListener("message", handleMessage);
     };
   }, [transformRef]);
-
 
   const pageSizeMap = {
     a4: {
@@ -174,7 +159,6 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
                 Back
               </Link>
             </div>
-<<<<<<< HEAD
             <div className="auth_section flex justify-end w-full gap-10 items-center">
             <Controls />
             <button
@@ -205,110 +189,78 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
                             width={500}
                             height={500}
                           />
-=======
-            <div className="auth_section flex justify-end w-full gap-10">
-              <Controls />
-              <button
-                className="2xl:p-5 md:p-2 p-2 bg-blue-900 text-white disabled:bg-gray-600 font-semibold 2xl:text-base md:text-sm text-[12px] flex items-center justify-around rounded-md"
-                onClick={handleDownloadResume}
-                disabled={isLoading}
-              >
-                {isLoading && (
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                )}
-                Download PDF
-              </button>
-              <div className="choose_templates xl:block hidden">
-                <Drawer direction="right">
-                  <DrawerTrigger className="bg-blue-900 text-white 2xl:p-5 md:p-2 p-1 2xl:text-base md:text-sm text-[12px] font-semibold rounded-md">
-                    Templates <LuLayoutGrid className="inline" />
-                  </DrawerTrigger>
-                  <DrawerContent className="bg-white flex flex-col h-full w-[500px] mt-24 fixed right-0">
-                    <DrawerHeader>
-                      <DrawerTitle>Choose Templates</DrawerTitle>
-                      <DrawerDescription>
-                        <div className="grid grid-cols-2 gap-5 overflow-y-scroll h-screen no-scrollbar">
-                          <div className="image_section_1" onClick={() => handleChangeTemplate('Template3')}>
-                            <Image
-                              src="/newResume.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_2" onClick={() => handleChangeTemplate('Template2')}>
-                            <Image
-                              src="/newResume1.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_1" onClick={() => handleChangeTemplate('Template1')}>
-                            <Image
-                              src="/newResume2.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_2">
-                            <Image
-                              src="/6.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_1">
-                            <Image
-                              src="/5.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_2">
-                            <Image
-                              src="/6.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_1">
-                            <Image
-                              src="/5.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
-                          <div className="image_section_2">
-                            <Image
-                              src="/6.png"
-                              alt="pic1"
-                              className="cursor-pointer hover:border-sky-700 hover:border-4"
-                              width={500}
-                              height={500}
-                            />
-                          </div>
->>>>>>> 3c992da823832daefc77c3ccc82f51696486cf00
                         </div>
-                      </DrawerDescription>
-                    </DrawerHeader>
-                  </DrawerContent>
-                </Drawer>
-              </div>
+                        <div className="image_section_2">
+                          <Image
+                            src="/newResume1.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_1">
+                          <Image
+                            src="/newResume2.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_2">
+                          <Image
+                            src="/6.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_1">
+                          <Image
+                            src="/5.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_2">
+                          <Image
+                            src="/6.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_1">
+                          <Image
+                            src="/5.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                        <div className="image_section_2">
+                          <Image
+                            src="/6.png"
+                            alt="pic1"
+                            className="cursor-pointer hover:border-sky-700 hover:border-4"
+                            width={500}
+                            height={500}
+                          />
+                        </div>
+                      </div>
+                    </DrawerDescription>
+                  </DrawerHeader>
+                </DrawerContent>
+              </Drawer>
             </div>
-
+            </div>
+            
             <div className="profile_section">
               <div className="ml-auto flex items-center px-6 lg:ml-4 lg:p-0">
                 {/* Avatar with Dropdown */}
@@ -355,7 +307,7 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
                         <div
                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
                           role="menuitem"
-                        // onClick={handleLogout}
+                          // onClick={handleLogout}
                         >
                           Logout
                         </div>
@@ -378,7 +330,7 @@ const ResumeViewPage = ({ resumeData, setResumeData }) => {
                   overflow: "hidden",
                 }}
               >
-                <GetTemplate name={resumeData?.metadata?.template} resumeData={resumeData} />
+                <Template3 resumeData={resumeData} />
               </div>
             </div>
           </TransformComponent>
