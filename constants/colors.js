651576db4af8cc4e49ec4057300f1b67a90ed685
manoj7417 <manoj@glassfrog.design>
2024-05-24 14:57:54 +0530

    Changes in ui

--- constants/colors.js
+++ constants/colors.js
@@ -1,21 +1,46 @@
 export const colors = [
-    "#475569", // slate-600
-    "#57534e", // stone-600
-    "#dc2626", // red-600
-    "#ea580c", // orange-600
-    "#d97706", // amber-600
-    "#ca8a04", // yellow-600
-    "#65a30d", // lime-600
-    "#16a34a", // green-600
-    "#059669", // emerald-600
-    "#0d9488", // teal-600
-    "#0891b2", // cyan-600
-    "#0284c7", // sky-600
-    "#2563eb", // blue-600
-    "#4f46e5", // indigo-600
-    "#7c3aed", // violet-600
-    "#9333ea", // purple-600
-    "#c026d3", // fuchsia-600
-    "#db2777", // pink-600
-    "#e11d48", // rose-600
+
+  "#8B1540", // Claret
+  "#8B1B65", // Flirt
+  "#9554C8", // Purple Plum
+  "#5B7FC5", // Glaucous
+  "#3A8DEF", // Blue De France
+  "#87CEFA", // Maye Blue
+  "#15ABAB", // Light Sea Green
+  "#6DB56D", // Bud Green
+  "#ACD68B", // Granny Smith Apple
+  "#9333ea", // purple-600
+  "#c026d3", // fuchsia-600
+  "#db2777", // pink-600
+  "#e11d48", // rose-600
+  "#AF0000", //Red
+  "#E46C6C", //Light Coral
+  "#660F0F", //Dark Red
+  "#00305C", //Dark Blue
+  "#3797BA", //Steel Blue
+  "#B0CCF1", //Powder Blue
+  "#D48F23", //Dark Goldenrod
+  "#E8AF8B", //Light Salmon
+  "#2047A8", //Royal Blue
+  "#021F69", //Dark Blue
+  "#B178BB", //Medium Purple
+  "#530062", //Dark Magenta
+  "#AF0079", //Strong Pink
+  "#E18BC7", //Light Pink
+  "#FFD9F3", //Pale Pink
+  "#004B05", //Dark Green
+  "#4D8951", //Sea Green
+  "#75CFCC", //Light Sea Green
+  "#0C8B87", //Dark Cyan
+  "#E8DB84", //Light Gold
+  "#000000", //Black
+];
+
+export const AccordianColor = [
+  "#F9BC2F", // Orange Yelow
+  "#FD8D30", // Cadmium Orange
+  "#F4613E", // Orange Soda
+  "#FF5E5E", // Orange Red Crayola
+  "#E13F79", // Mystic,
+  "#FFB6C1", // Light Pink
 ];
