ce0a029bcb4abed68f5966d3668529e5206a404c
manoj7417 <manoj@glassfrog.design>
2024-06-03 12:29:06 +0530

    changes in builder

--- components/component/service-section.jsx
+++ components/component/service-section.jsx
@@ -112,97 +112,16 @@ export function ServiceSection() {
               </div>
             </div>
           </div>
-          <div className=" px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2"></div>
-            </div>
-            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-              {/* <Image
-                alt="Image"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-first"
-                height="310"
-                src="/new_section3.png"
-                width="550"
-              /> */}
-              {/* <div className="lg:w-[550px] w-full h-auto shadow-lg rounded-md pr-10">
-                <video
-                  width="1000"
-                  height="1000"
-                  autoPlay
-                  muted
-                  loop
-                  preload="none"
-                >
-                  <source src="/aigenie.mp4" type="video/mp4" />
-                  <track
-                    src="/path/to/captions.vtt"
-                    kind="subtitles"
-                    srcLang="en"
-                    label="English"
-                  />
-                  Your browser does not support the video tag.
-                </video>
-              </div> */}
-              {/* <div className="flex flex-col justify-center space-y-4 lg:ps-20 ps-1">
-                <div className="px-0 flex items-center rounded-2xl text-center py-1 ">
-                  <h2 className="lg:text-4xl text-3xl font-bold text-blue-900">
-                    CV Analyzer
-                  </h2>
-                  <GiMagicLamp className="h-14 w-14 ms-5 text-blue-900" />
-                </div>
-                <ul className="grid gap-6 mb-5">
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-blue-950">
-                        ATS score
-                      </h3>
-                      <p className="text-gray-500 text-sm">
-                      Find out how well your CV performs with Applicant Tracking Systems (ATS). Our tool provides a detailed score, pinpointing areas for improvement to ensure your CV gets through automated screenings. Get practical tips to enhance your CV&apos;s structure, content, and formatting, making it more appealing to both ATS and hiring managers.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-blue-950">
-                        Suggestion for CV
-                      </h3>
-                      <p className="text-gray-500 text-sm">
-                      Get expert recommendations to perfect your CV. Our tool reviews your CV and offers personalized suggestions to make it more effective. Whether it&apos;s adjusting the format, refining the content, or highlighting key skills, get professional advice to make your CV stand out.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-blue-950">
-                        Optimize your CV
-                      </h3>
-                      <p className="text-gray-500 text-sm">
-                      Enhance your CV with our comprehensive optimization service. Our tool checks for errors, ensures contextual accuracy, and provides targeted improvements. From content alignment to skill presentation, optimize every aspect of your CV to increase your chances of landing your desired job.
-                      </p>
-                    </div>
-                  </li>
-                </ul>
-                <div>
-                  <button className="items-center flex px-7 py-2 rounded-md text-base text-white bg-blue-900 hover:bg-blue-700">
-                    <a className="flex items-center" href="/genie-dashboard">
-                      Analyze Now
-                      <GiMagicLamp className="h-6 w-6 ms-5 text-white" />
-                    </a>
-                  </button>
-                </div>
-              </div> */}
-            </div>
-          </div>
         </section>
 
         <section
-          className="w-full py-12 md:py-24 lg:py-32 border-t lg:px-20  scroll-mt-20 bg-gradient-to-r from-[white] to-[#dcecff]"
-          style={{
-            backgroundImage: "url('/banner-bg.svg')",
-            backgroundPosition: "center",
-          }}
+          className="w-full py-12 md:py-24 lg:py-32 border-t lg:px-20  scroll-mt-20 bg-gradient-to-r from-gray-100 to-[#cad7e8]"
+          // style={{
+          //   backgroundImage: "url('/banner-bg.svg')",
+          //   backgroundPosition: "center",
+          // }}
         >
-          <div className="container mx-auto grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
+          <div className="container mx-auto lg:max-w-[75rem] w-full grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
             <div className="space-y-2">
               <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
                 Looking for More?
@@ -219,12 +138,6 @@ export function ServiceSection() {
               >
                 Contact Expert
               </Link>
-              {/* <Link
-                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50    "
-                href="#"
-              >
-                Learn more
-              </Link> */}
             </div>
           </div>
         </section>
