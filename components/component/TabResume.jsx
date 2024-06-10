668c046dd9608a1fd0cf58726c7fb4008243c19f
amanglassfrog <aman@glassfrog.design>
2024-06-10 15:56:15 +0530

    fix layout

--- components/component/TabResume.jsx
+++ components/component/TabResume.jsx
@@ -418,163 +418,62 @@ const TabResume = () => {
 
   return (
     <>
-      <div className="bg-gradient-to-b from-white to-[#2C98CA33]">
-        <div className="rounded-t-xl p-6 shadow-xl">
-          <div className="tabs_heading">
-            <h2 className="2xl:text-6xl lg:text-5xl text-3xl font-bold mt-5 tracking-tighter text-gray-900 text-center">
-              Discover CV that fits your job role
-            </h2>
-            <p className="w-1/2 mx-auto text-center my-4">
-              Explore unlimited possibilities with the power of a perfectly
-              crafted CV by creating one that aligns with your Professional
-              Profile, employing our customised Curriculum Vitae templates.{" "}
-            </p>
-          </div>
-
-          <Tabs
-            className="max-w-5xl mx-auto py-5"
-            defaultValue="Business Analyst"
-          >
-            <div className="grid grid-cols-2 place-items-center">
-              <div className="tabs_main">
-                <TabsList className="mb-4 flex w-full justify-start flex-wrap py-10 h-auto">
-                  {TabsHeader?.map((item, index) => (
-                    <TabsTrigger value={item?.name} key={index}>
-                      <div className="tabs_header flex gap-2 items-center justify-start">
-                        {item?.icon}
-                        {item?.name}
-                      </div>
-                    </TabsTrigger>
-                  ))}
-                  {/* <TabsTrigger value="all">
-                    <FaBorderAll className="text-pink-600 h-8 w-8 me-3" />
-                    All templates
-                  </TabsTrigger>
-                  <TabsTrigger value="simple">
-                    <PiFolderSimpleUser className="text-blue-700 h-8 w-8 me-3" />
-                    Simple
-                  </TabsTrigger>
-                  <TabsTrigger value="ats">
-                    <MdQueryStats className="text-orange-600 h-8 w-8 me-3" />
-                    Ats
-                  </TabsTrigger>
-                  <TabsTrigger value="designer">
-                    <IoShirt className="text-green-700 h-8 w-8 me-3" />
-                    Designer
-                  </TabsTrigger>
-                  <TabsTrigger value="professional">
-                    <RiShirtFill className="text-red-700 h-8 w-8 me-3" />
-                    Professional
-                  </TabsTrigger> */}
-                </TabsList>
-              </div>
-              <div className="tabs_content">
-                {TabsHeader?.map((item, index) => (
-                  <TabsContent value={item?.name} key={index}
-                    <div className="group relative">
-                      <Tilt
-                        options={defaultOptions}
-                        style={{ height: 500, width: 350 }}
-                      >
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={400}
-                          height={400}
-                          alt={item.name}
-                          className="border-4 rounded-md border-gray-300 bg-gray-300 p-4"
-                        />
-                        <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                          <Button className="inline-flex h-10 items-center justify-center rounded-md bg-[#0EA5E9] px-8 text-sm font-medium text-white shadow transition-colors hover:bg-[#0284C7] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:opacity-100 disabled:bg-[#82cdf0]">
-                            Try Now
-                          </Button>
-                        </div>
-                      </Tilt>
-                    </div>
-                  </TabsContent>
-                ))}
-                {/* <TabsContent className="py-7" value="all">
-                  <div>
-                    {AllTemplates.map((item, index) => {
-                      return (
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={500}
-                          height={500}
-                          alt={item.name}
-                        />
-                      );
-                    })}
-                  </div>
-                </TabsContent> */}
-                {/* <TabsContent value="simple">
-                  <div>
-                    {SimpleTemplates?.map((item, index) => {
-                      return (
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={500}
-                          height={500}
-                          alt={item.name}
-                        />
-                      );
-                    })}
-                  </div>
-                </TabsContent>
-                <TabsContent value="ats">
-                  <div>
-                    {ATSTemplates?.map((item, index) => {
-                      return (
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={500}
-                          height={500}
-                          alt={item.name}
-                        />
-                      );
-                    })}
-                  </div>
-                </TabsContent>
-                <TabsContent value="designer">
-                  <div>
-                    {DesignerTemplates?.map((item, index) => {
-                      return (
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={500}
-                          height={500}
-                          alt={item.name}
-                        />
-                      );
-                    })}
-                  </div>
-                </TabsContent>
-                <TabsContent value="professional">
-                  <div>
-                    {ProfessionalTemplates?.map((item, index) => {
-                      return (
-                        <Image
-                          src={item.src}
-                          key={index}
-                          width={500}
-                          height={500}
-                          alt={item.name}
-                        />
-                      );
-                    })}
-                  </div>
-                </TabsContent> */}
-              </div>
-            </div>
-          </Tabs>
+  <div className="bg-gradient-to-b from-white to-[#2C98CA33]">
+      <div className="rounded-t-xl p-6 shadow-xl">
+        <div className="tabs_heading">
+          <h2 className="2xl:text-6xl lg:text-5xl text-3xl font-bold mt-5 tracking-tighter text-gray-900 text-center">
+            Discover CV that fits your job role
+          </h2>
+          <p className="w-1/2 mx-auto text-center my-4">
+            Explore unlimited possibilities with the power of a perfectly
+            crafted CV by creating one that aligns with your Professional
+            Profile, employing our customised Curriculum Vitae templates.
+          </p>
         </div>
+
+        <Tabs className="max-w-5xl mx-auto py-5" defaultValue="Business Analyst">
+          <div className="grid grid-cols-2 place-items-center">
+            <div className="tabs_main">
+              <TabsList className="mb-4 flex w-full justify-start flex-wrap py-10 h-auto">
+                {TabsHeader?.map((item, index) => (
+                  <TabsTrigger value={item?.name} key={index}>
+                    <div className="tabs_header flex gap-2 items-center justify-start">
+                      {item?.icon}
+                      {item?.name}
+                    </div>
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+            </div>
+            <div className="tabs_content">
+              {TabsHeader?.map((item, index) => (
+                <TabsContent value={item?.name} key={index}>
+                  <div className="group relative">
+                    <Tilt options={defaultOptions} style={{ height: 500, width: 350 }}>
+                      <Image
+                        src={item.src}
+                        key={index}
+                        width={400}
+                        height={400}
+                        alt={item.name}
+                        className="border-4 rounded-md border-gray-300 bg-gray-300 p-4"
+                      />
+                      <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <Button className="inline-flex h-10 items-center justify-center rounded-md bg-[#0EA5E9] px-8 text-sm font-medium text-white shadow transition-colors hover:bg-[#0284C7] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:opacity-100 disabled:bg-[#82cdf0]">
+                          Try Now
+                        </Button>
+                      </div>
+                    </Tilt>
+                  </div>
+                </TabsContent>
+              ))}
+            </div>
+          </div>
+        </Tabs>
       </div>
-    </>
-  );
+    </div>
+  </>
+);
 };
 
 export default TabResume;
