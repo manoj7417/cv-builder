204d6d3a43f7ddd5ffced45e74359bde23547bd6
manoj7417 <manoj@glassfrog.design>
2024-06-13 17:06:42 +0530

    Changes in Header

--- app/Layout/NewResumeHeader.jsx
+++ app/Layout/NewResumeHeader.jsx
@@ -19,7 +19,7 @@ const navigation = [
     href: "/resumeAnalyzer-dashboard",
     current: false,
   },
-  { name: "CV Match", href: "/coming-soon", current: false ,isBeta:true},
+  { name: "CV Match", href: "/jobCV", current: false ,isBeta:true},
 ];
 
 function classNames(...classes) {
