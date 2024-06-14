085c93112bf04f128b2f50982f1acbad77e87f4e
anujglassfrog <anujrawat@glassfrog.design>
2024-06-14 14:36:40 +0530

    subscribe feature integrated

--- components/component/service-section.jsx
+++ components/component/service-section.jsx
@@ -1,11 +1,47 @@
+'use client'
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { GiMagicLamp } from "react-icons/gi";
 import { FiBox } from "react-icons/fi";
+import { useRef, useState } from "react";
+import { sendSubscribeEmail } from "@/app/pages/api/api";
+import { GetTokens } from "@/app/actions";
+import { useRouter } from "next/navigation";
+import { useUserStore } from "@/app/store/UserStore";
 
 export function ServiceSection() {
-  
+  const [isSubscribing, setIsSubscribing] = useState(false)
+  const [isSubscribed, setIsSubscribed] = useState(false)
+  const email = useRef('')
+  const router = useRouter()
+  const { userdata } = useUserStore(state => state.userState)
+
+  const handleSubscribe = async () => {
+    const { accessToken } = await GetTokens()
+    if (!accessToken) {
+      router.push('/login')
+      return
+    }
+    setIsSubscribing(true)
+    const obj = {
+      name: userdata?.fullname,
+      email: email.current.value
+    }
+    try {
+      const response = await sendSubscribeEmail(obj)
+      console.log(response)
+      if (response.status === 200) {
+        setIsSubscribed(true)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      email.current.value = ''
+      setIsSubscribing(false)
+    }
+  }
+
   return (
     <div className="flex flex-col">
       <main className="flex-1">
@@ -220,23 +256,33 @@ export function ServiceSection() {
           <div className=" grid items-center justify-center gap-4 px-4 text-center md:px-6">
             <div className="space-y-3">
               <h2 className="text-4xl font-bold tracking-tighter md:text-5xl/tight">
-              Stay Ahead of the Curve!
+                Stay Ahead of the Curve!
               </h2>
               <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
                 Subscribe to our newsletter and get the latest updates.
               </p>
             </div>
             <div className="mx-auto w-full max-w-sm space-y-2">
-              <form className="flex space-x-2 mb-5">
+              <div className="flex space-x-2 mb-5">
                 <input
-                  className="max-w-lg flex-1 px-3 bg-blue-900 bg-opacity-10 rounded-lg text-black placeholder-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                  className="max-w-lg flex-1 px-3 bg-blue-900 bg-opacity-10 rounded-lg text-black placeholder-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-gray-300 opacity-55"
+                  
                   placeholder="Enter your email"
                   type="email"
+                  ref={email}
+                  disabled={isSubscribed}
                 />
-                <div className="inline-flex h-10 items-center justify-center rounded-md bg-blue-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 ">
-                  Subscribe
-                </div>
-              </form>
+                {
+                  isSubscribed ?
+                    <Button className="inline-flex h-10 items-center justify-center rounded-md bg-green-700 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-green-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 " >
+                      Susbcribed
+                    </Button>
+                    :
+                    <Button className="inline-flex h-10 items-center justify-center rounded-md bg-blue-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50" disabled={isSubscribing} onClick={handleSubscribe}>
+                      {isSubscribing ? 'Subscribing..' : 'Subscribe'}
+                    </Button>
+                }
+              </div>
               <p className="text-xs text-gray-500">
                 <Link className="underline underline-offset-2" href="/terms-condition">
                   Terms & Conditions
