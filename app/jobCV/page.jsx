bd95254fc161b12ed186e58e2268f187b20a4e22
Shubham Kumar <shubham@glassfrog.design>
2024-06-14 16:13:06 +0530

    jobcv page updated

--- app/jobCV/page.jsx
+++ app/jobCV/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import Header from "../Layout/Header";
@@ -9,20 +9,23 @@ import SearchBar from "../ui/Searchbar";
 import { RiAiGenerate } from "react-icons/ri";
 import TabMenu from "../ui/TabMenu";
 import { Dialog } from "@radix-ui/react-dialog";
-import { DialogContent, } from "@/components/ui/dialog";
+import { DialogContent } from "@/components/ui/dialog";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { FaWpforms } from "react-icons/fa";
 import JobMultistepForm from "@/components/component/JobMultistepForm";
 import { toast } from "react-toastify";
 import { useResumeStore } from "../store/ResumeStore";
 import { useRouter } from "next/navigation";
-import pdfToText from 'react-pdftotext'
+import pdfToText from "react-pdftotext";
 import { GetTokens } from "../actions";
 import { generateResumeOnFeeback } from "../pages/api/api";
 import Lottie from "lottie-react";
-import animation from '@/public/animations/JobCVLoader.json'
+import animation from "@/public/animations/JobCVLoader.json";
+import CountUp from "react-countup";
 
-const NewResumeHeader = dynamic(() => import('../Layout/NewResumeHeader'), { ssr: false });
+const NewResumeHeader = dynamic(() => import("../Layout/NewResumeHeader"), {
+  ssr: false,
+});
 
 const options = [
   "Software Engineer",
@@ -114,9 +117,8 @@ const options = [
   "Therapist",
   "Counselor",
   "Social Worker",
-  "Community Service Manager"
+  "Community Service Manager",
 ];
-
 
 const jobTabs = [
   {
@@ -354,48 +356,48 @@ const jobTabs = [
   },
 ];
 
-
-
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
-  const [jobRole, setJobRole] = useState('')
+  const [jobRole, setJobRole] = useState("");
   const userState = useUserStore((state) => state.userState);
-  const [showDialog, setShowDialog] = useState(false)
-  const [showMultiStepDialog, setShowMultiStepDialog] = useState(false)
-  const [steps, setSteps] = useState(1)
-  const inputRef = useRef()
-  const { userdata } = useUserStore(state => state.userState)
-  const replaceResumeData = useResumeStore((state) => state.replaceResumeData)
-  const router = useRouter()
-  const updateUserData = useUserStore(state => state.updateUserData)
-  const [generatingResume, setIsGeneratingResume] = useState(false)
+  const [showDialog, setShowDialog] = useState(false);
+  const [showMultiStepDialog, setShowMultiStepDialog] = useState(false);
+  const [steps, setSteps] = useState(1);
+  const inputRef = useRef();
+  const { userdata } = useUserStore((state) => state.userState);
+  const replaceResumeData = useResumeStore((state) => state.replaceResumeData);
+  const router = useRouter();
+  const updateUserData = useUserStore((state) => state.updateUserData);
+  const [generatingResume, setIsGeneratingResume] = useState(false);
   const [formData, setFormData] = useState({
-    fullname: userdata?.fullname || '',
-    email: userdata?.email || '',
+    fullname: userdata?.fullname || "",
+    email: userdata?.email || "",
     jobTitle: "",
-    country: '',
-    city: '',
+    country: "",
+    city: "",
     isFresher: false,
     experience: [],
     skills: [],
     education: [],
-    projects: []
-  })
-
+    projects: [],
+  });
 
   const handleDialogClose = () => {
-    setShowDialog(false)
-  }
+    setShowDialog(false);
+  };
 
   const handlefileupload = () => {
-    inputRef.current.click()
-  }
+    inputRef.current.click();
+  };
 
   const fetchBetterResume = async (message, accessToken) => {
-    message = message + `generate resume for this ${jobRole}`
+    message = message + `generate resume for this ${jobRole}`;
 
     try {
-      const response = await generateResumeOnFeeback(message, accessToken.value);
+      const response = await generateResumeOnFeeback(
+        message,
+        accessToken.value
+      );
       if (response.status === 201) {
         return response.data;
       }
@@ -406,69 +408,70 @@ export default function Home() {
 
   const handleuploadResume = (e) => {
     let selectedFile = e.target.files[0];
-    if (selectedFile.type !== 'application/pdf') return toast.error("Please select a valid PDF file")
+    if (selectedFile.type !== "application/pdf")
+      return toast.error("Please select a valid PDF file");
     let reader = new FileReader();
     reader.readAsDataURL(selectedFile);
-    setShowDialog(false)
+    setShowDialog(false);
     reader.onloadend = async () => {
       try {
-        const text = await pdfToText(selectedFile)
+        const text = await pdfToText(selectedFile);
         if (!text) {
-          toast.error("Non ATS friendly resume found")
+          toast.error("Non ATS friendly resume found");
           return;
         }
-        setIsGeneratingResume(true)
-        const { accessToken } = await GetTokens()
+        setIsGeneratingResume(true);
+        const { accessToken } = await GetTokens();
         if (!accessToken) {
-          router.push('/login?redirect=/jobCV')
+          router.push("/login?redirect=/jobCV");
           return;
         }
         const { data, userData } = await fetchBetterResume(text, accessToken);
         if (data) {
-          replaceResumeData(data)
-          updateUserData(userData)
+          replaceResumeData(data);
+          updateUserData(userData);
           router.push("/builder");
         }
       } catch (error) {
         toast.error("Unable to generate your CV");
       } finally {
-        setIsGeneratingResume(false)
+        setIsGeneratingResume(false);
       }
     };
-  }
+  };
 
   const handleCloseMultistepForm = () => {
-    setShowMultiStepDialog(false)
+    setShowMultiStepDialog(false);
     setFormData({
-      fullname: userdata?.fullname || '',
-      email: userdata?.email || '',
+      fullname: userdata?.fullname || "",
+      email: userdata?.email || "",
       jobTitle: "",
-      country: '',
-      city: '',
+      country: "",
+      city: "",
       isFresher: false,
       experience: [],
       skills: [],
       education: [],
-      projects: []
-    })
-  }
+      projects: [],
+    });
+  };
 
   const handleDontHaveCV = () => {
-    setShowDialog(false)
-    setShowMultiStepDialog(true)
-  }
+    setShowDialog(false);
+    setShowMultiStepDialog(true);
+  };
 
   const handleGenerateNow = async () => {
     if (!jobRole) {
-      return toast.error("Please select a job role")
+      return toast.error("Please select a job role");
     }
-    const { accessToken } = await GetTokens()
+    const { accessToken } = await GetTokens();
     if (!accessToken) {
-      router.push('/login?redirect=/jobCV')
+      router.push("/login?redirect=/jobCV");
       return;
     }
-    setShowDialog(true)
-  }
+    setShowDialog(true);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -485,12 +488,24 @@ export default function Home() {
           <Dialog open={showDialog}>
             <DialogContent onClick={handleDialogClose}>
               <div className="w-full flex">
-                <div className="w-1/2 h-48 shadow-lg flex flex-col items-center justify-center cursor-pointer rounded-md mr-5 hover:scale-105 duration-300 delay-400" onClick={handlefileupload}>
+                <div
+                  className="w-1/2 h-48 shadow-lg flex flex-col items-center justify-center cursor-pointer rounded-md mr-5 hover:scale-105 duration-300 delay-400"
+                  onClick={handlefileupload}
+                >
                   <IoDocumentTextOutline className="text-9xl text-blue-900" />
                   Upload CV
-                  <input type="file" hidden ref={inputRef} accept="application/pdf" onChange={handleuploadResume} />
+                  <input
+                    type="file"
+                    hidden
+                    ref={inputRef}
+                    accept="application/pdf"
+                    onChange={handleuploadResume}
+                  />
                 </div>
-                <div className="w-1/2 h-48  shadow-lg flex flex-col items-center justify-center cursor-pointer rounded-md hover:scale-105 duration-300 delay-400" onClick={handleDontHaveCV}>
+                <div
+                  className="w-1/2 h-48  shadow-lg flex flex-col items-center justify-center cursor-pointer rounded-md hover:scale-105 duration-300 delay-400"
+                  onClick={handleDontHaveCV}
+                >
                   <FaWpforms className="text-9xl text-blue-900" />
                   Dont have a CV?
                 </div>
@@ -498,41 +513,106 @@ export default function Home() {
             </DialogContent>
           </Dialog>
           <Dialog open={showMultiStepDialog}>
-            <JobMultistepForm showMultiStepDialog={showMultiStepDialog} onClick={handleCloseMultistepForm} steps={steps} setSteps={setSteps} formData={formData} setFormData={setFormData} jobRole={jobRole} />
+            <JobMultistepForm
+              showMultiStepDialog={showMultiStepDialog}
+              onClick={handleCloseMultistepForm}
+              steps={steps}
+              setSteps={setSteps}
+              formData={formData}
+              setFormData={setFormData}
+              jobRole={jobRole}
+            />
           </Dialog>
           <Dialog open={generatingResume}>
             <DialogContent onClick showCloseButton>
-              <div className='mx-auto flex items-center flex-col'>
-                <Lottie animationData={animation} className='w-[300px] h-[300px]' />
-                <p className='mt-1'>Preparing your CV</p>
+              <div className="mx-auto flex items-center flex-col">
+                <Lottie
+                  animationData={animation}
+                  className="w-[300px] h-[300px]"
+                />
+                <p className="mt-1">Preparing your CV</p>
               </div>
             </DialogContent>
           </Dialog>
           <div className="flex justify-center items-center px-36">
             <div className="flex flex-col">
-              <h1 className="font-extrabold text-5xl pe-20 mb-6">Generate the <span className="text-blue-500">CV</span> that will land you the job you desire!</h1>
+              <h1
+                className="font-extrabold text-[3.9rem] pe-20 mb-6"
+                style={{ lineHeight: "1.3 !important" }}
+              >
+                Build a <span style={{ color: "#1b2f93" }}>CV</span> that opens
+                doors to your ideal{" "}
+                <span style={{ color: "#1b2f93" }}>career</span>!
+              </h1>
               <div className="grid grid-cols-2 gap-10   py-3 rounded-lg">
                 <div>
-                  <SearchBar jobRole={jobRole} setJobRole={setJobRole} options={options} />
+                  <SearchBar
+                    jobRole={jobRole}
+                    setJobRole={setJobRole}
+                    options={options}
+                  />
                 </div>
                 <div>
-
-                  <button className="bg-blue-700 text-white px-3 py-2 rounded-lg flex items-center gap-2" onClick={() => handleGenerateNow()}>Generate Now <RiAiGenerate className="text-xl font-bold" /></button>
+                  <button
+                    className="bg-blue-700 text-white px-3 py-2 rounded-lg flex items-center gap-2"
+                    onClick={() => handleGenerateNow()}
+                  >
+                    Generate Now <RiAiGenerate className="text-xl font-bold" />
+                  </button>
                 </div>
               </div>
             </div>
             <div>
-
-              <Image src="/recruitment-concept-with-laptop.png" width={600} height={700} alt="Job CV" />
+              <Image
+                src="/cvgenerator.png"
+                width={1300}
+                height={700}
+                alt="Job CV"
+                style={{ height: "19rem !important" }}
+              />
             </div>
-
           </div>
+        </section>
+        <section>
+          <div className="container mx-auto py-12 px-4 md:px-36 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center bg-[#f5f5f5] p-12 rounded-3xl">
+              <div className="text-center md:text-left ">
+                <h2
+                  className="text-2xl md:text-[2.7rem] font-bold mb-4"
+                  style={{ lineHeight: "1.4 !important" }}
+                >
+                  Empower Yourself With Knowledge, Anytime, Anywhere !
+                </h2>
+                <button
+                  className=" text-black px-4 py-2 rounded"
+                  style={{ border: "1px solid black" }}
+                >
+                  <a href="/contact-us">Work With Coach</a>
+                </button>
+              </div>
+              <div className="flex justify-around items-center md:items-center h-[60%] bg-white w-[100%] rounded-3xl">
+                <div className="text-4xl font-bold text-black">
+                  <CountUp start={1} end={200} duration={2} />+
+                  <p className="text-lg font-semibold">Templates</p>
+                </div>
 
+                <div className="text-4xl font-bold text-black ">
+                  <CountUp start={1} end={40} duration={2} />
+                  k+
+                  <p className="text-lg font-semibold">Users</p>
+                </div>
+
+                <div className="text-4xl font-bold text-black ">
+                  <CountUp start={1} end={50} duration={2} />+
+                  <p className="text-lg font-semibold">Coaches</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="bg-white text-black">
           <div className="container mx-auto p-4">
-          <TabMenu tabs={jobTabs} />
-
+            <TabMenu tabs={jobTabs} />
           </div>
         </section>
         <Footer />
