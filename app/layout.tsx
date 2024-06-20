0535f5a8216c015f4b4d2070b9353c12f1ee87de
manoj7417 <manoj@glassfrog.design>
2024-06-20 16:42:23 +0530

    Changes in ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
-import Header from "./Layout/Header";
-import Footer from "./Layout/Footer";
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
 
