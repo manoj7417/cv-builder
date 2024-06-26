952f2809b8ce969a83db0177bb40f0f1a6dd5023
Shubham Kumar <shubham@glassfrog.design>
2024-06-26 14:04:56 +0530

    pshycometric-test page added

---
+++ app/pshycometric-test/layout.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Layout/Footer"
+
+const Layout = ({ children }) => {
+    return (
+      <main className="resume_dashboard" >
+        <div className="resume_dashboard_container">
+          <div className="wrapper">
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </main>
+    )
+  }
+  
+  export default Layout
