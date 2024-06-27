636549fe147a506b97db7a35ab3fc26e96d15586
anujglassfrog <anujrawat@glassfrog.design>
2024-06-27 16:10:19 +0530

    payment flow updated

---
+++ app/api/CheckPaymentStatus/route.js
@@ -0,0 +1,23 @@
+import { serverInstance } from '@/lib/serverApi';
+
+export async function POST(req, res) {
+    try {
+        const { sessionId } = await req.json();
+        const token = req.headers.get('Authorization');
+        const response = await serverInstance.post('/stripe/check-payment-status', { sessionId }, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        return new Response(JSON.stringify(response.data), {
+            status: 201,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({ error: "Error generating feedback" }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+}
