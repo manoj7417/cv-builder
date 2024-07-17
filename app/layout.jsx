190425776f76867d4ee9611c2a29c9561e08be58
anujglassfrog <anujrawat@glassfrog.design>
2024-07-17 18:03:00 +0530

    ( test ) : testing layout in prod

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-loading-skeleton/dist/skeleton.css";
 
 export const metadata = {
   title: "Professional CV Templates for Perfect CVs on Genies CV Maker",
@@ -12,16 +12,12 @@ export const metadata = {
     "Find best Curriculum Vitae Template and Career Assistance on top CV Maker and CV Library Website, Genies Career Hub and create your CV best fit for Job Role.",
 };
 
-const inter = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// const inter = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
+//className={inter.className}
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
         <ToastContainer />
       </body>
