aa7abd961f2085813e89949953ce4fc798112887
anujglassfrog <anujrawat@glassfrog.design>
2024-07-18 17:10:15 +0530

    ( fix ) : autoprefixer module added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 // import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "./context/AuthContext";
+
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -18,7 +18,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
         <ToastContainer />
       </body>
     </html>
