7da155e7bd31796af078dbce5bdecc6b9e7aaeb5
manoj7417 <manoj@glassfrog.design>
2024-07-19 12:11:51 +0530

    Changes in layout js

--- components/component/NewSlider.jsx
+++ components/component/NewSlider.jsx
@@ -3,11 +3,6 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import ImageCarousel from "./ImageCarousel";
