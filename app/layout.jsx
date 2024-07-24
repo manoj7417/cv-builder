8f4e663b9b0ff04de137a7dfeff3a8201c57a340
anujglassfrog <anujrawat@glassfrog.design>
2024-07-24 13:10:43 +0530

    (feat ) : meta script added

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,29 @@ export default function RootLayout({ children }) {
   gtag('config', 'G-3864YFJZRV');
             `,
           }} />
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WSQGHKPJ');`}} />
         <meta name="google-site-verification" content="61LbG4ASq8urm_ZEHIr2jlNt13kGnN8n921g2EJF5po" />
+        <script dangerouslySetInnerHTML={{
+          __html: ` !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '452806670914263');
+          fbq('track', 'PageView');`}} />
+        <noscript><img height="1" width="1" style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=452806670914263&ev=PageView&noscript=1"
+        /></noscript>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSQGHKPJ"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
       </head>
       <body className={inter.className}>
         <ToastContainer />
