ac5835e2c2ad264454f09b6ae574fd693dbcbdc2
manoj7417 <manoj@glassfrog.design>
2024-08-13 17:06:29 +0530

    Changes in career counselling pages

--- app/career-counselling/layout.jsx
+++ app/career-counselling/layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
       <main className="resume_dashboard" >
         <div className="resume_dashboard_container">
           <div className="wrapper">
-            <NewResumeHeader/>
+            {/* <NewResumeHeader/> */}
             {children}
             <Footer/>
           </div>
