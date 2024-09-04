022a86f319a12dcd3599fd8c078f6fd58b98d0af
JagdishGlassfrog <jagdish@glassfrog.design>
2024-09-04 10:50:51 +0530

    coach appointment calender logic has almost completed

---
+++ app/(career-coach)/coach-dashboard/page.jsx
@@ -0,0 +1,18 @@
+'use client';
+import React, { useState } from "react";
+
+
+
+const CoachDashboardPage = () => {
+
+
+  return (
+    <>
+      <div>
+        Dashboard
+      </div>
+    </>
+  );
+};
+
+export default CoachDashboardPage;
