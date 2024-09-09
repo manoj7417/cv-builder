114e91a0d13ba1e447050c30234d4534000ff268
manoj7417 <manoj@glassfrog.design>
2024-09-09 16:16:14 +0530

    Changes in login and sign up page

--- app/(career-coach)/coach-registration/page.jsx
+++ app/(career-coach)/coach-registration/page.jsx
@@ -60,8 +60,15 @@ export default function CoachRegistration() {
   };
 
   return (
-    <section>
+    <section className="max-w-7xl mx-auto">
       <div className="grid grid-cols-1 lg:grid-cols-2 my-20">
+        <div className="h-full w-full lg:block md:block hidden">
+          <img
+            className="mx-auto h-full w-full rounded-md object-cover"
+            src="https://images.unsplash.com/photo-1559526324-4b87b5e36e44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+            alt=""
+          />
+        </div>
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
@@ -314,13 +321,7 @@ export default function CoachRegistration() {
             </form>
           </div>
         </div>
-        <div className="h-full w-full">
-          <img
-            className="mx-auto h-full w-full rounded-md object-cover"
-            src="https://images.unsplash.com/photo-1559526324-4b87b5e36e44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
-            alt=""
-          />
-        </div>
+        
       </div>
     </section>
   );
