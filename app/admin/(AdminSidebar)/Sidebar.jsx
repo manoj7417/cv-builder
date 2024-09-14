ae41ba41547688ceab7c5e8a8d2b5d5a82b83f13
manoj7417 <manoj@glassfrog.design>
2024-09-14 11:27:20 +0530

    Changes in admin dashboard

--- app/admin/(AdminSidebar)/Sidebar.jsx
+++ app/admin/(AdminSidebar)/Sidebar.jsx
@@ -7,6 +7,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoMdClose } from "react-icons/io";
 import { HiMenu } from "react-icons/hi";
+import { FaKey } from "react-icons/fa";
+import { LuUserSquare2 } from "react-icons/lu";
+import { IoDocumentsOutline } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,122 +30,110 @@ const Sidebar = () => {
   // Function to apply active class based on activeTab
   const getLinkClass = (tab) => {
     return activeTab === tab
-      ? "flex items-center p-3 rounded-full bg-white text-blue-500 cursor-pointer"
-      : "flex items-center p-3 rounded-full hover:bg-white hover:text-blue-500 cursor-pointer";
+      ? "flex items-center p-3 rounded-md bg-[#1D4ED8] text-white cursor-pointer"
+      : "flex items-center p-3 text-gray-500 rounded-md hover:bg-[#1D4ED8] hover:text-white cursor-pointer";
   };
 
   return (
-    <div className='flex lg:h-full h-auto lg:sticky lg:top-0 relative'>
+    <div className="flex lg:h-full h-auto lg:sticky lg:top-0 relative p-5">
       {/* Mobile Hamburger Menu Button */}
       <button
-        className='md:hidden p-4 focus:outline-none'
-        onClick={toggleSidebar}>
-        {/* <Image
-          src="/icons/hamburger-icon.png" // Replace with your hamburger icon path
-          alt="Menu"
-          width={30}
-          height={30}
-        /> */}
-        <HiMenu className='w-8 h-8' />
+        className="md:hidden p-4 focus:outline-none"
+        onClick={toggleSidebar}
+      >
+        <HiMenu className="w-8 h-8" />
       </button>
 
       {/* Sidebar for Mobile and Desktop */}
       <div
         className={`fixed inset-y-0 left-0 transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
-        } md:relative md:translate-x-0 bg-[#E0F2FF] p-4 transition-transform duration-300 ease-in-out z-50 w-full`}>
+        } md:relative md:translate-x-0 bg-white p-4 transition-transform duration-300 ease-in-out z-50 w-full`}
+      >
         {/* Close button for mobile sidebar */}
-        <div className='flex justify-end mr-5'>
+        <div className="flex justify-end mr-5">
           <button
-            className='md:hidden mb-4 focus:outline-none cursor-pointer'
-            onClick={toggleSidebar}>
-            <IoMdClose className='w-8 h-8' />
+            className="md:hidden mb-4 focus:outline-none cursor-pointer"
+            onClick={toggleSidebar}
+          >
+            <IoMdClose className="w-8 h-8" />
           </button>
         </div>
-
-        {/* Profile Section */}
-        <div className='flex items-center mb-6 w-full'>
-          <div className='w-20 h-auto overflow-hidden rounded-full'>
-            <Image
-              src='/coach_photos.png' // Replace with your profile image path
-              alt='Profile Image'
-              width={64} // Width should match the container's width
-              height={64} // Height should match the container's height
-              className='object-cover'
-            />
-          </div>
-          <div className='ml-2'>
-            <p className='font-semibold text-gray-800'>Kevin Gilbert</p>
-            <p className='text-gray-600'>Web Designer Coach</p>
-          </div>
+        <div className="dashboard_logo my-5">
+          <Image src="/admin-logo.png" alt="logo" width={200} height={200} />
         </div>
-
-        <nav>
-          <ul className='space-y-4 lg:w-full w-[65%]'>
+        {/* Links for the pages  */}
+        <nav className="px-5 py-10 space-y-5">
+          <ul className="space-y-4 lg:w-full w-[65%]">
             {/* Highlighted Dashboard Item with Custom Icon */}
             <li className={getLinkClass("/admin")}>
               <Link
-                href='/admin'
-                className='flex items-center'
-                onClick={() => handleSetActiveTab("/admin")}>
-                <Image
-                  src='/coach_dashboard.png' // Replace with your custom icon path
-                  alt='Dashboard Icon'
-                  width={24}
-                  height={24}
-                  className='mr-3'
-                />
-                <span className='font-semibold text-gray-800'>Dashboard</span>
+                href="/admin"
+                className="flex items-center"
+                onClick={() => handleSetActiveTab("/admin")}
+              >
+                <FaKey className="w-4 h-4 mr-4" />
+                <span className="font-semibold">Dashboard</span>
               </Link>
             </li>
             <li className={getLinkClass("/admin/coach")}>
               <Link
                 href={"/admin/coach"}
                 onClick={() => handleSetActiveTab("/admin/coach")}
-                className='flex items-center'>
-                <Image
-                  src='/coach_personal_detail_icon.png' // Replace with your custom icon path
-                  alt='Personal Details Icon'
-                  width={24}
-                  height={24}
-                  className='mr-3'
-                />
-                <span className='font-semibold text-gray-800'>Coach</span>
+                className="flex items-center"
+              >
+                <LuUserSquare2 className="w-6 h-6 mr-4" />
+                <span className="font-semibold">Coach</span>
               </Link>
             </li>
             {/* Other Items with Custom Icons */}
             <li className={getLinkClass("/admin/viewBlogs")}>
               <Link
-                href='/admin/viewBlogs'
-                className='flex items-center'
-                onClick={() => handleSetActiveTab("/admin/viewBlogs")}>
-                <Image
-                  src='/blog_icon.png' // Replace with your custom icon path
-                  alt='Blog Icon'
-                  width={24}
-                  height={24}
-                  className='mr-3'
-                />
-                <span className='font-semibold text-gray-800'>Blog</span>
+                href="/admin/viewBlogs"
+                className="flex items-center"
+                onClick={() => handleSetActiveTab("/admin/viewBlogs")}
+              >
+                <IoDocumentsOutline className="w-6 h-6 mr-4" />
+                <span className="font-semibold ">Blog</span>
               </Link>
             </li>
             <li className={getLinkClass("/admin/viewUsers")}>
               <Link
                 href={"/admin/viewUsers"}
-                className='flex items-center'
-                onClick={() => handleSetActiveTab("/admin/viewUsers")}>
-                <Image
-                  src='/coach_personal_detail_icon.png' // Replace with your custom icon path
-                  alt='Appointments Icon'
-                  width={24}
-                  height={24}
-                  className='mr-3'
-                />
-                <span className='font-semibold text-gray-800'>User</span>
+                className="flex items-center"
+                onClick={() => handleSetActiveTab("/admin/viewUsers")}
+              >
+                <LuUserSquare2 className="w-6 h-6 mr-4" />
+                <span className="font-semibold">User</span>
               </Link>
             </li>
           </ul>
         </nav>
+        {/* Profile Section */}
+        <div className="flex items-center justify-between mb-6 w-full absolute bottom-0 px-5">
+          <div className="image_section flex items-center">
+            <div className="w-16 h-auto overflow-hidden rounded-full">
+              <Image
+                src="/coach_photos.png" // Replace with your profile image path
+                alt="Profile Image"
+                width={52} // Width should match the container's width
+                height={52} // Height should match the container's height
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-grow">
+              <p className="font-semibold text-gray-800 text-base">
+                Kevin Gilbert
+              </p>
+              <p className="text-gray-600 text-sm">Web Designer Coach</p>
+            </div>
+          </div>
+          <div className="logout">
+            <button className="text-gray-600 hover:text-blue-700">
+              <FiLogOut className="w-6 h-6" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -13,20 +13,20 @@ const Layout = ({ children }) => {
         <div className='wrapper'>
           <div id='mainDiv' className='flex flex-col h-screen'>
             {/* START-HEADER */}
-            <AdminHeader className='w-full' />
+            {/* <AdminHeader className='w-full' /> */}
             {/* END-HEADER */}
             <div className='flex flex-1 flex-col md:flex-row'>
               {/* Sidebar */}
               <div
                 id='sidebarCoach'
-                className='w-full md:w-[20%] lg:w-[20%] bg-[#E0F2FF] sticky top-0 lg:h-screen h-auto z-50'>
+                className='w-full md:w-[20%] lg:w-[20%] bg-white sticky top-0 lg:h-screen h-auto z-50'>
                 {/* Sidebar content goes here */}
                 <Sidebar className='w-full' />
               </div>
               {/* Main Content */}
               <div
                 id='mainContent'
-                className='w-full md:w-[80%] lg:w-[80%] bg-white z-10'>
+                className='w-full md:w-[80%] lg:w-[80%]  bg-[#E0F2FF] bg-gradient-to-r from-[#D9EBFE] via-[#D9EBFE] to-white  z-10'>
                 {/* Coach Dashboard content goes here */}
                 {children}
               </div>
