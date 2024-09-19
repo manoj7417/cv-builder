a88020c1144783e231944967b955040c165e59b9
manoj7417 <manoj@glassfrog.design>
2024-09-19 17:48:44 +0530

    Changes in coach form

---
+++ app/api/verifyDocs/route.js
@@ -0,0 +1,30 @@
+import { serverInstance } from '@/lib/serverApi';
+
+export async function PATCH(req, res) {
+    try {
+        const token = req.headers.get('Authorization');
+        const payload = await req.json();
+        console.log("payload::", payload);
+        const {id,...otherData} = payload;
+        const response = await serverInstance.patch(`/admin/verifyCoach/${id}`, otherData, {
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(first)
+        return new Response(JSON.stringify(response.data), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : { error: "Error updating in coach data" };
+        const statusCode = error.response ? error.response.status : 500;
+        return new Response(JSON.stringify(errorMessage), {
+            status: statusCode,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+}
--- app/store/coachDetailStore.js
+++ app/store/coachDetailStore.js
@@ -1,28 +1,45 @@
 // useCoachesStore.js
-import { create } from 'zustand';
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-const useCoachesDetailStore = create((set) => ({
-  coaches: [],
-  singleCoach: {},
-  isLoading: false,
-  fetchAllCoaches: async () => {
-    set({ isLoading: true });
-    try {
-      const response = await fetch('/api/getAllCoaches');
-      const data = await response.json();
-      set({ coaches: data.coaches });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      set({ isLoading: false });
-    }
-  },
+const myMiddlewares = (f) => devtools(persist(f, { name: "coachDetail" }));
 
-  // Filter and set a single coach by id
-  filterCoachById: (id) => set((state) => {
-    const coach = state.coaches.find((coach) => coach._id === id);
-    return { singleCoach: coach};
-  }),
-}));
+const useCoachesDetailStore = create(
+  myMiddlewares((set) => ({
+    coaches: [],
+    singleCoach: {},
+    isLoading: false,
+
+    // Fetch all coaches
+    fetchAllCoaches: async () => {
+      set({ isLoading: true });
+      try {
+        const response = await fetch("/api/getAllCoaches");
+        const data = await response.json();
+        set({ coaches: data.coaches });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+
+    // Filter and set a single coach by id
+    filterCoachById: (id) =>
+      set((state) => {
+        const coach = state.coaches.find((coach) => coach._id === id);
+        return { singleCoach: coach };
+      }),
+
+    // Update singleCoach by merging existing properties with new ones
+    updateSingleCoach: (updatedFields) =>
+      set((state) => ({
+        singleCoach: {
+          ...state.singleCoach,
+          ...updatedFields, // Merges the existing singleCoach with new updated fields
+        },
+      })),
+  }))
+);
 
 export default useCoachesDetailStore;
