cca5e1738d6ade8b017eaa344ef48505b5330e54
amanglassfrog <aman@glassfrog.design>
2024-09-27 15:53:08 +0530

    test

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -27,7 +27,6 @@ import { loadRazorpayScript } from "../utils/razorpayUtils";
 import { Button } from "@/components/ui/button";
 import { useRouter, useSearchParams } from "next/navigation";
 
-
 const PricingFunc = () => {
   const [selectedCard, setSelectedCard] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
