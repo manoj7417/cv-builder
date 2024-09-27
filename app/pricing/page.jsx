e365b91f142b38ac9defa0c9b61ed1f83e9cc086
amanglassfrog <aman@glassfrog.design>
2024-09-27 14:29:13 +0530

    test

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -42,6 +42,7 @@ const PricingFunc = () => {
     timezone: "",
     currency: "",
   });
+  
   const searchParams = useSearchParams();
   const scroll = searchParams.get("scroll");
   const router = useRouter();
