bac9f7f5432d61f221ae6d2b54971ac6b1184bde
anujglassfrog <anujrawat@glassfrog.design>
2024-10-11 17:26:26 +0530

    ( feat ) : edit coach request page added

---
+++ app/api/(admin)/getEditCoachRequests/route.js
@@ -0,0 +1,23 @@
+import { serverInstance } from '@/lib/serverApi';
+
+export async function GET(req, res) {
+    try {
+        const token = req.headers.get('Authorization');
+        const response = await serverInstance.get(`/admin/getEditCoachRequests`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        return new Response(JSON.stringify(response.data), {
+            status: response.status || 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : { error: "Error getting edit coach requests" };
+        const statusCode = error.response ? error.response.status : 500;
+        return new Response(JSON.stringify(errorMessage), {
+            status: statusCode,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+}
