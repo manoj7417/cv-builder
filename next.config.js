b539d180f18be1e17e939fde41aaad298e6a7959
amanglassfrog <aman@glassfrog.design>
2024-11-04 11:04:38 +0530

    test

--- next.config.js
+++ next.config.js
@@ -38,6 +38,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   webpack: (config) => {
@@ -65,7 +71,7 @@ const nextConfig = {
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.youtube.com https://www.youtube.com/iframe_api;
               style-src 'self' 'unsafe-inline';
-              img-src 'self' data: blob: https://res.cloudinary.com https://static.vecteezy.com https://via.placeholder.com https://geniescareerhubbucket.lon1.digitaloceanspaces.com https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com https://uxwing.com http://localhost:3000 https://geniescareerhub.com;
+              img-src 'self' data: blob: https://res.cloudinary.com https://static.vecteezy.com https://via.placeholder.com https://geniescareerhubbucket.lon1.digitaloceanspaces.com https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com https://uxwing.com https://upload.wikimedia.org http://localhost:3000 https://geniescareerhub.com;
               font-src 'self' data:;
               connect-src 'self' https://goldfish-app-a2e3u.ondigitalocean.app http://localhost:3000 https://geniescareerhub.com https://www.google-analytics.com https://ipapi.co;
               frame-src 'self' https://www.youtube.com;
