801cb212d1fcd600c12760f2cc27971ca2c9eba4
amanglassfrog <aman@glassfrog.design>
2024-11-04 15:31:47 +0530

    test

--- app/api/getAllCoaches/route.js
+++ app/api/getAllCoaches/route.js
@@ -1,16 +1,15 @@
+import { serverInstance } from "@/lib/serverApi";
+
 export async function GET(req, res) {
     try {
         const response = await serverInstance.get('/coach/all');
-        
         return new Response(JSON.stringify(response.data), {
             status: response.status || 200,
-            headers: {
-                'Content-Type': 'application/json',
-                'Cache-Control': 'no-store' // Disable caching
-            }
+            headers: { 'Content-Type': 'application/json' }
         });
     } catch (error) {
-        const errorMessage = error.response ? error.response.data : { error: "Error forgetting password" };
+        console.log('Error in /api/getAllCoaches:', error.message); // Detailed logging
+        const errorMessage = error.response ? error.response.data : { error: "An error occurred in fetching coaches" };
         const statusCode = error.response ? error.response.status : 500;
         return new Response(JSON.stringify(errorMessage), {
             status: statusCode,
