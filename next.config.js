1c0ec1882ffcc9713cd524c047e2d389a33e6265
amanglassfrog <aman@glassfrog.design>
2024-12-12 16:14:35 +0530

    test

--- next.config.js
+++ next.config.js
@@ -85,10 +85,10 @@ const nextConfig = {
             style-src 'self' 'unsafe-inline';
             img-src 'self' data: blob: https://res.cloudinary.com https://static.vecteezy.com https://via.placeholder.com https://geniescareerhubbucket.lon1.digitaloceanspaces.com https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com https://uxwing.com https://upload.wikimedia.org http://localhost:3000 https://geniescareerhub.com https://c.clarity.ms https://lh3.googleusercontent.com https://images.unsplash.com https://www.linkedin.com;
             font-src 'self' data:;
-            connect-src 'self' blob: https://goldfish-app-a2e3u.ondigitalocean.app http://localhost:3000 https://geniescareerhub.com https://www.google-analytics.com https://ipapi.co ws://127.0.0.1:51673 https://l.clarity.ms https://i.clarity.ms https://r.clarity.ms https://api.stripe.com;
+            connect-src 'self' blob: https://goldfish-app-a2e3u.ondigitalocean.app http://localhost:3000 https://geniescareerhub.com https://www.google-analytics.com https://ipapi.co ws://127.0.0.1:51673 https://l.clarity.ms https://i.clarity.ms https://r.clarity.ms https://m.clarity.ms https://c.clarity.ms https://api.stripe.com;
             frame-src 'self' https://www.youtube.com https://docs.google.com https://js.stripe.com;
             worker-src 'self' blob:;
-          `.trim().replace(/\s+/g, ' '),
+          `.trim().replace(/\s+/g, ' '),          
           },
           {
             key: "Referrer-Policy",
