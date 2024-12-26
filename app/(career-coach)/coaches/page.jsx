1d467f6dc05a54ada77172bccfc95983c787b493
manoj7417 <manoj@glassfrog.design>
2024-12-26 15:12:01 +0530

    Changes in coaches program

--- app/(career-coach)/coaches/page.jsx
+++ app/(career-coach)/coaches/page.jsx
@@ -18,6 +18,7 @@ import { useRouter } from "next/navigation";
 
 const CoachPage = () => {
   const [coaches, setAllCoaches] = useState([]);
+  console.log("coaches::",coaches)
   const [isLoading, setIsLoading] = useState(true);
   const [selectedCoach, setSelectedCoach] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
@@ -135,7 +136,7 @@ const CoachPage = () => {
       <div className="max-w-7xl mx-auto mt-[150px] mb-10 px-4">
         <div className="coach_main_div w-full flex flex-col lg:flex-row gap-10">
           {/* Coach Selection Section */}
-          <div className="coach_card lg:w-[30%] w-full h-auto lg:max-h-[500px] lg:sticky top-[100px] lg:overflow-y-scroll overflow-hidden">
+          <div className="coach_card lg:w-[30%] w-full h-auto lg:max-h-[500px] lg:sticky top-[100px] lg:overflow-y-scroll overflow-hidden pr-2">
             <h2 className="text-xl lg:text-2xl font-bold">
               Choose the <span className="text-blue-700">coach</span> who aligns
               best with your <span className="text-blue-700">goals.</span>
@@ -155,7 +156,11 @@ const CoachPage = () => {
                     .filter(
                       (coach) =>
                         coach.isApproved && coach.approvalStatus === "approved"
-                    )
+                    ).sort((a, b) => {
+                      const hasProgramsA = a.programs?.length > 0;
+                      const hasProgramsB = b.programs?.length > 0;
+                      return hasProgramsB - hasProgramsA;
+                    })
                     .map((item, index) => (
                       <div
                         key={index}
@@ -177,7 +182,7 @@ const CoachPage = () => {
                               <h3 className="text-base font-medium text-slate-900">
                                 {item.name}
                               </h3>
-                              <p className="text-xs text-slate-800 text-wrap">
+                              <p className="text-xs text-slate-800 break-all">
                                 {item?.email}
                               </p>
                               <Button
@@ -374,7 +379,7 @@ const CoachPage = () => {
             </DialogTitle>
           </DialogHeader>
           <div>
-            {selectedCoach?.programs.length > 0 ? (
+            {selectedCoach?.programs.length > 0 && selectedCoach?.programs?.isapproved === true ? (
               <>
                 <div className="grid lg:grid-cols-2 grid-cols-1 gap-10">
                   <div className="coach_related_programs">
