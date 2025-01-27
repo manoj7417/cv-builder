7da66c7eaf5ad5f6a39280dfb4552497952d4c95
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2025-01-27 17:49:15 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -53,64 +53,67 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
         <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-KJ9G6JTK"
-        ></script>
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KJ9G6JTK');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Remove duplicate GTM script */}
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SCXB98QM5R');`,
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SCXB98QM5R');`
           }}
-        ></script>
+        />
+
+        {/* Microsoft Clarity */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "otnsy3oksi");
-          `,
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "otnsy3oksi");
+            `
           }}
         />
+
+        {/* Hotjar */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KJ9G6JTK');
-          `,
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5279200,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`
           }}
         />
-        {/*  Hotjar Tracking Code for https://www.geniescareerhub.com/ */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: ` (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5279200,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
-          }}
-        ></script>
+      </head>
+      <body suppressHydrationWarning={true} className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KJ9G6JTK"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
         </noscript>
-      </head>
-      <body suppressHydrationWarning={true} className={inter.className}>
+        {/* End Google Tag Manager (noscript) */}
+
         <SessionProviderWrapper>
           <ToastContainer
             position="top-right"
@@ -125,7 +128,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             theme="colored"
             transition={Slide}
           />
-
           {children}
         </SessionProviderWrapper>
       </body>
