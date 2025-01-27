15558a2ed7c83499f0270f8a7ad966ebff849e25
manoj7417 <manoj@glassfrog.design>
2025-01-27 16:56:11 +0530

    Changes in google tagmanager

--- app/layout.jsx
+++ app/layout.jsx
@@ -84,7 +84,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KJ9G6JTK')
+})(window,document,'script','dataLayer','GTM-KJ9G6JTK');
           `,
           }}
         />
@@ -122,6 +122,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               src="https://www.googletagmanager.com/ns.html?id=GTM-KJ9G6JTK"
               height="0"
               width="0"
+              style="display:none;visibility:hidden"
             ></iframe>
           </noscript>
           {children}
