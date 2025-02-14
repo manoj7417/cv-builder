ad02e6f193c8ca5348ff6849718ef820d54d659c
manoj7417 <manoj@glassfrog.design>
2025-02-14 14:41:26 +0530

    Add meta title anddescription in authnetication pages

--- app/(authentication)/register/layout.jsx
+++ app/(authentication)/register/layout.jsx
@@ -1,5 +1,8 @@
 export const metadata = {
   metadataBase: new URL("https://www.geniescareerhub.com"),
+  title: "Register Yourself With Genies Career Hub Account",
+  description:
+    " Create your professional CV online. Or, get your CV ATS optimized at a click. Register your account with the Genies Career Hub Account using simple steps.",
   alternates: {
     canonical: "https://www.geniescareerhub.com/register",
   },
--- app/jobs/layout.jsx
+++ app/jobs/layout.jsx
@@ -4,6 +4,9 @@ import { ResumeHeader } from "@/app/Layout/ResumeHeader";
 import Footer from "../Layout/Footer";
 export const metadata = {
   metadataBase: new URL("https://www.geniescareerhub.com"),
+  title: "Find Your Dream Jobs - Genies Career Hub",
+  description:
+    "Finding dream jobs is made easy with the interactive interface by Genies Career Hub. Now, incorporated with filters to land the best suitable job profile.",
   alternates: {
     canonical: "https://www.geniescareerhub.com/jobs",
   },
