9131e2390e311c4f07d5e5baaf3adc16194d5af8
manoj7417 <manoj@glassfrog.design>
2025-03-29 16:34:24 +0530

    Changes doen

--- app/components/HomepageNew/index.jsx
+++ app/components/HomepageNew/index.jsx
@@ -308,7 +308,6 @@ export default function HomepageNew() {
               <p className="text-gray-500 text-sm max-w-xs">
                 {feature.description}
               </p>
-              <h1>dksfjslkdfjlksdj</h1>
             </div>
           ))}
         </div>
